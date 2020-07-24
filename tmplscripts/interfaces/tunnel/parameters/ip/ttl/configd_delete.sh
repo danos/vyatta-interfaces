d96d42394822ac28d4fb5e1b11a1768b21846e2d
Thomas Kiely <tkiely@mail.eng.vyatta.net>
2020-07-24 10:35:53 +0100

    Remove restriction on setting ttl and nopmtudisc.
    This restriction is legacy and is not enforced by
    "ip link".
    
    VRVDR-52221
    
    (cherry picked from commit 3652b5c8e9857ffcac4a10e1df46ddb6e84b2376)

--- tmplscripts/interfaces/tunnel/parameters/ip/ttl/configd_delete.sh
+++ tmplscripts/interfaces/tunnel/parameters/ip/ttl/configd_delete.sh
@@ -1,10 +1,4 @@
 #!/opt/vyatta/bin/cliexec
 type=$(vyatta-tunnel-encap-to-type $VAR(../../../encapsulation/@))
 
-if [ -n "$VAR(../../../path-mtu-discovery-disable/@)" ]; then
-    TTL="ttl inherit";
-else
-    TTL="ttl 255";
-fi
-
-ip link set $VAR(../../../@) type $type $TTL
+ip link set $VAR(../../../@) type $type ttl 255
